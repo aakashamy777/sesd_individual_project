826bb0e7e4fe7d619e2872f71f3b6ce6a7099399
Aakash <aakshmehta2005@gmail.com>
2026-04-19 21:35:11 +0530

    03: Prisma client and configuration

---
+++ prisma.config.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import path from "path";
+import { defineConfig } from "prisma/config";
+
+dotenv.config({
+  path: path.resolve(process.cwd(), ".env"),
+  override: true,
+});
+
+console.log("DB URL:", process.env.DATABASE_URL);
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
