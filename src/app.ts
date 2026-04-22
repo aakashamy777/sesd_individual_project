b16c2b882f7964a2d3e3162556e217be0f57086f
Aakash <aakshmehta2005@gmail.com>
2026-04-22 10:08:36 +0530

    debug: add request logging to backend

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+    console.log(`[BACKEND-LOG] ${req.method} ${req.url}`);
+    next();
+});
+
 app.use("/api/v1", routes);
 
 app.use(notFoundHandler);
