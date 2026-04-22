e4db89ca035ef4584438e7c4030c54df6fa7ca23
Aakash <aakshmehta2005@gmail.com>
2026-04-22 11:48:04 +0530

    fix: migrate to Prisma 7 configuration and Driver Adapter

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
+import { env } from "./env";
 
-export const prisma = new PrismaClient();
+const pool = new pg.Pool({ connectionString: env.databaseUrl });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({ adapter });
